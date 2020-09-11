ded331f0a55477db7c5f69bf9d19578850f07a90
Patricia Salazar <patricia.escorche@gmail.com>
2020-09-10 23:14:44 -0400

    Components

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default Header;
+
+
